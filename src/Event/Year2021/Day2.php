d3bf86f2603bd7a9f80d7d107cc9ea6c3891a61a
Georgi Georgiev <vuryss@gmail.com>
2021-12-02 07:10:22 +0200

    Year 2021 Day 2

---
+++ src/Event/Year2021/Day2.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Event\Year2021;
+
+use App\Event\DayInterface;
+
+class Day2 implements DayInterface
+{
+    public function testPart1(): iterable
+    {
+        yield '150' => <<<'INPUT'
+            forward 5
+            down 5
+            forward 8
+            up 3
+            down 8
+            forward 2
+            INPUT;
+    }
+
+    public function testPart2(): iterable
+    {
+        yield '900' => <<<'INPUT'
+            forward 5
+            down 5
+            forward 8
+            up 3
+            down 8
+            forward 2
+            INPUT;
+    }
+
+    public function solvePart1(string $input): string
+    {
+        $commands = explode("\n", $input);
+        $depth = $pos = 0;
+
+        foreach ($commands as $command) {
+            [$command, $value] = explode(' ', $command);
+
+            match ($command) {
+                'forward' => $pos += (int) $value,
+                'up' => $depth -= (int) $value,
+                'down' => $depth += (int) $value,
+            };
+        }
+
+        return (string) ($depth * $pos);
+    }
+
+    public function solvePart2(string $input): string
+    {
+        $commands = explode("\n", $input);
+        $depth = $pos = $aim = 0;
+
+        foreach ($commands as $command) {
+            [$command, $value] = explode(' ', $command);
+
+            switch ($command) {
+                case 'forward':
+                    $pos += (int) $value;
+                    $depth += $aim * (int) $value;
+                    break;
+                case 'up':
+                    $aim -= (int) $value;
+                    break;
+                case 'down':
+                    $aim += (int) $value;
+                    break;
+            }
+        }
+
+        return (string) ($depth * $pos);
+    }
+}
