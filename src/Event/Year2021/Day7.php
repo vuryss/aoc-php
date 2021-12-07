b005b18986c071a1e41e7c8b9ff11c0b90592682
Georgi Georgiev <vuryss@gmail.com>
2021-12-07 07:16:17 +0200

    Year 2021 Day 7

---
+++ src/Event/Year2021/Day7.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Event\Year2021;
+
+use App\Event\DayInterface;
+
+class Day7 implements DayInterface
+{
+    public function testPart1(): iterable
+    {
+        yield '37' => <<<'INPUT'
+            16,1,2,0,4,2,7,1,2,14
+            INPUT;
+    }
+
+    public function testPart2(): iterable
+    {
+        yield '168' => <<<'INPUT'
+            16,1,2,0,4,2,7,1,2,14
+            INPUT;
+    }
+
+    public function solvePart1(string $input): string
+    {
+        $positions = array_map('intval', explode(',', $input));
+        $min = min($positions);
+        $max = max($positions);
+        $fuel = PHP_INT_MAX;
+
+        for ($i = $min; $i <= $max; $i++) {
+            $sum = 0;
+
+            foreach ($positions as $position) {
+                $sum += abs($position - $i);
+            }
+
+            if ($sum < $fuel) {
+                $fuel = $sum;
+            }
+        }
+
+        return (string) $fuel;
+    }
+
+    public function solvePart2(string $input): string
+    {
+        $positions = array_map('intval', explode(',', $input));
+        $min = min($positions);
+        $max = max($positions);
+        $fuel = PHP_INT_MAX;
+
+        for ($i = $min; $i <= $max; $i++) {
+            $sum = 0;
+
+            foreach ($positions as $position) {
+                $steps = abs($position - $i);
+                $sum += $steps * ($steps + 1) / 2;
+            }
+
+            if ($sum < $fuel) {
+                $fuel = $sum;
+            }
+        }
+
+        return (string) $fuel;
+    }
+}
