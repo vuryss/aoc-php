c54c10ba22e834700c793a041a00a6fb14886a67
Georgi Georgiev <vuryss@gmail.com>
2021-12-24 11:30:56 +0200

    Year 2021 Day 24

--- src/Event/Year2021/Day24.php
+++ src/Event/Year2021/Day24.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Event\Year2021;
 
 use App\Event\DayInterface;
+use Closure;
 
 class Day24 implements DayInterface
 {
@@ -20,37 +21,15 @@ class Day24 implements DayInterface
 
     public function solvePart1(string $input): string|int
     {
-        $lines = explode("\n", $input);
-        $parts = array_chunk($lines, 18);
-
-        $stack = [];
-        $number = array_fill_keys(range(1, 14), 0);
-
-        foreach ($parts as $index => $part) {
-            $num1 = (int) explode(' ', $part[5])[2];
-            $num2 = (int) explode(' ', $part[15])[2];
-
-            if ($num1 > 9) {
-                $stack[] = ['index' => $index, 'offset' => $num2];
-                continue;
-            }
-
-            $value = array_pop($stack);
-            $diff = $value['offset'] + $num1;
-
-            if ($diff > 0) {
-                $number[$index + 1] = 9;
-                $number[$value['index'] + 1] = 9 - $diff;
-            } else {
-                $number[$value['index'] + 1] = 9;
-                $number[$index + 1] = 9 + $diff;
-            }
-        }
-
-        return implode('', $number);
+        return $this->solve($input, Closure::fromCallable([$this, 'maximize']));
     }
 
     public function solvePart2(string $input): string|int
+    {
+        return $this->solve($input, Closure::fromCallable([$this, 'minimize']));
+    }
+
+    private function solve(string $input, callable $diffFn): string
     {
         $lines = explode("\n", $input);
         $parts = array_chunk($lines, 18);
@@ -68,17 +47,30 @@ class Day24 implements DayInterface
             }
 
             $value = array_pop($stack);
-            $diff = $value['offset'] + $num1;
+            $result = $diffFn($value['offset'] + $num1);
 
-            if ($diff > 0) {
-                $number[$index + 1] = 1 + $diff;
-                $number[$value['index'] + 1] = 1;
-            } else {
-                $number[$value['index'] + 1] = 1 - $diff;
-                $number[$index + 1] = 1;
-            }
+            $number[$index + 1] = $result[0];
+            $number[$value['index'] + 1] = $result[1];
         }
 
         return implode('', $number);
+    }
+
+    private function maximize(int $diff): array
+    {
+        if ($diff > 0) {
+            return [9, 9 - $diff];
+        }
+
+        return [9 + $diff, 9];
+    }
+
+    private function minimize(int $diff): array
+    {
+        if ($diff > 0) {
+            return [1 + $diff, 1];
+        }
+
+        return [1, 1 - $diff];
     }
 }
