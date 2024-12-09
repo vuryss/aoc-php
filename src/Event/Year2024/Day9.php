c309a966710a3ffb4489085cb922795e1c66e3f3
Georgi Georgiev <vuryss@gmail.com>
2024-12-09 07:51:22 +0200

    Year 2024, Day 9 optimized part 1

--- src/Event/Year2024/Day9.php
+++ src/Event/Year2024/Day9.php
@@ -24,40 +24,31 @@ class Day9 implements DayInterface
 
     public function solvePart1(string $input): string|int
     {
-        $chars = array_map(intval(...), str_split($input));
+        $chars = str_split($input);
         $disk = [];
         $fileIndex = 0;
 
         foreach ($chars as $index => $digit) {
-            if ($index % 2 === 0) {
-                array_push($disk, ...array_fill(0, $digit, $fileIndex++));
-            } else {
-                array_push($disk, ...array_fill(0, $digit, null));
-            }
-        }
-
-        for ($i = count($disk) - 1; $i >= 0; $i--) {
-            if ($disk[$i] === null) {
-                continue;
-            }
-
-            for ($j = 0; $j < count($disk); $j++) {
-                if ($j === $i) {
-                    break 2;
-                }
-
-                if ($disk[$j] === null) {
-                    $disk[$j] = $disk[$i];
-                    $disk[$i] = null;
-                    break;
-                }
-            }
+            array_push($disk, ...array_fill(0, (int) $digit, $index % 2 === 0 ? $fileIndex++ : null));
         }
 
         $sum = 0;
+        $reverseIndex = count($disk) - 1;
 
-        foreach ($disk as $index => $fileIndex) {
-            $sum += $index * $fileIndex;
+        for ($i = 0; $i <= $reverseIndex; $i++) {
+            $value = $disk[$i];
+
+            if ($value === null) {
+                for ($j = $reverseIndex; $j > $i; $j--) {
+                    if ($disk[$j] !== null) {
+                        $value = $disk[$j];
+                        $reverseIndex = $j - 1;
+                        break;
+                    }
+                }
+            }
+
+            $sum += $i * $value;
         }
 
         return $sum;
