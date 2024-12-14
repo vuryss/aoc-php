adc455c62ac13b962a2d8b582a6535a9cd39af07
Georgi Georgiev <vuryss@gmail.com>
2024-12-14 15:09:54 +0200

    Year 2024, Day 14 - Alternate solution that should work for everybody

--- src/Event/Year2024/Day14.php
+++ src/Event/Year2024/Day14.php
@@ -76,22 +76,40 @@ class Day14 implements DayInterface
 
         foreach (explode("\n", $input) as $line) {
             $ints = StringUtil::extractIntegers($line);
-            $robots[] = ['x' => $ints[0], 'y' => $ints[1], 'vx' => $ints[2], 'vy' => $ints[3]];
+            $robots[] = $ints;
         }
 
         while (++$seconds) {
-            $set = [];
+            $grid = [];
+            $verticalPoints = [];
 
-            foreach ($robots as $i => $robot) {
-                $robots[$i]['x'] += $robot['vx'];
-                $robots[$i]['x'] = ($robots[$i]['x'] % $gridWidth + $gridWidth) % $gridWidth;
-                $robots[$i]['y'] += $robot['vy'];
-                $robots[$i]['y'] = ($robots[$i]['y'] % $gridHeight + $gridHeight) % $gridHeight;
-
-                $set["{$robots[$i]['x']},{$robots[$i]['y']}"] = true;
+            foreach ($robots as $i => [$x, $y, $vx, $vy]) {
+                $robots[$i][0] = (($x + $vx) % $gridWidth + $gridWidth) % $gridWidth;
+                $robots[$i][1] = (($y + $vy) % $gridHeight + $gridHeight) % $gridHeight;
+                $verticalPoints[$robots[$i][0]] = ($verticalPoints[$robots[$i][0]] ?? 0) + 1;
+                $grid[$robots[$i][1]][$robots[$i][0]] = true;
             }
 
-            if (count($set) === count($robots)) {
+            $maxColumn = max($verticalPoints);
+
+            if ($maxColumn < 30) {
+                continue;
+            }
+
+            $key = array_search($maxColumn, $verticalPoints);
+            $maxConsecutive = 0;
+            $consecutive = 0;
+
+            for ($y = 0; $y < $gridHeight - 2; $y++) {
+                if (isset($grid[$y][$key]) && isset($grid[$y + 1][$key])) {
+                    $consecutive++;
+                } else {
+                    $maxConsecutive = max($maxConsecutive, $consecutive);
+                    $consecutive = 0;
+                }
+            }
+
+            if ($maxConsecutive > 30) {
                 return $seconds;
             }
         }
