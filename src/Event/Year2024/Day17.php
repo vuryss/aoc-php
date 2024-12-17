cbf7b9fff895eef4685acf22c7938354e11c1855
Georgi Georgiev <vuryss@gmail.com>
2024-12-17 08:51:08 +0200

    Year 2024, Day 17

---
+++ src/Event/Year2024/Day17.php
@@ -0,0 +1,144 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Event\Year2024;
+
+use App\Event\DayInterface;
+
+class Day17 implements DayInterface
+{
+    public function testPart1(): iterable
+    {
+        yield '4,6,3,5,6,3,5,2,1,0' => <<<'INPUT'
+            Register A: 729
+            Register B: 0
+            Register C: 0
+            
+            Program: 0,1,5,4,3,0
+            INPUT;
+    }
+
+    public function testPart2(): iterable
+    {
+        yield '117440' => <<<'INPUT'
+            Register A: 2024
+            Register B: 0
+            Register C: 0
+            
+            Program: 0,3,5,4,3,0
+            INPUT;
+    }
+
+    public function solvePart1(string $input): string|int
+    {
+        $blocks = explode("\n\n", $input);
+        $registers = [];
+
+        foreach (explode("\n", $blocks[0]) as $line) {
+            preg_match('/Register ([A-Z]): (\d+)/', $line, $matches);
+            $registers[$matches[1]] = (int)$matches[2];
+        }
+
+        $blocks[1] = str_replace('Program: ', '', $blocks[1]);
+        $program = array_map('intval', explode(',', $blocks[1]));
+
+        return implode(',', $this->execute($registers, $program));
+    }
+
+    public function solvePart2(string $input): string|int
+    {
+        $blocks = explode("\n\n", $input);
+        $registers = [];
+        $blocks[1] = str_replace('Program: ', '', $blocks[1]);
+        $program = array_map('intval', explode(',', $blocks[1]));
+
+        foreach (explode("\n", $blocks[0]) as $line) {
+            preg_match('/Register ([A-Z]): (\d+)/', $line, $matches);
+            $registers[$matches[1]] = (int)$matches[2];
+        }
+
+        $search = array_reverse($program);
+        $a = 0;
+        $lastRemoved = null;
+
+        while (true) {
+            $registers['A'] = $a;
+            $output = $this->execute($registers, $program);
+
+            if ($output[0] === $search[0] && ($lastRemoved === null || $lastRemoved === $output[1])) {
+                $lastRemoved = array_shift($search);
+
+                if ([]  === $search) {
+                    break;
+                }
+
+                $a <<= 3;
+
+                continue;
+            }
+
+            $a++;
+        }
+
+        return $a;
+    }
+
+    private function execute(array $registers, array $program): array
+    {
+        $output = [];
+        $pointer = 0;
+
+        while ($pointer < count($program)) {
+            $instruction = $program[$pointer];
+            $comboOperand = $program[$pointer + 1];
+            $value = match (true) {
+                $comboOperand <= 3 => $comboOperand,
+                $comboOperand === 4 => $registers['A'],
+                $comboOperand === 5 => $registers['B'],
+                $comboOperand === 6 => $registers['C'],
+            };
+
+            switch ($instruction) {
+                case 0:
+                    $registers['A'] = intdiv($registers['A'], 2 ** $value);
+                    break;
+
+                case 1:
+                    $registers['B'] ^= $comboOperand;
+                    break;
+
+                case 2:
+                    $registers['B'] = $value % 8;
+                    break;
+
+                case 3:
+                    if ($registers['A'] !== 0) {
+                        $pointer = $comboOperand;
+                        continue 2;
+                    }
+                    break;
+
+                case 4:
+                    $registers['B'] ^= $registers['C'];
+                    break;
+
+                case 5:
+                    $output[] = $value % 8;
+                    break;
+
+                case 6:
+                    $registers['B'] = intdiv($registers['A'], 2 ** $value);
+                    break;
+
+                case 7:
+                    $registers['C'] = intdiv($registers['A'], 2 ** $value);
+                    break;
+            }
+
+            $pointer += 2;
+        }
+
+        return $output;
+    }
+}
