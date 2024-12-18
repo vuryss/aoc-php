cf5b1f59dfd4a038891a39985f9c042dc9916bd7
Georgi Georgiev <vuryss@gmail.com>
2024-12-18 07:51:58 +0200

    Year 2024, Day 18

--- src/Event/Year2024/Day18.php
+++ src/Event/Year2024/Day18.php
@@ -84,37 +84,9 @@ class Day18 implements DayInterface
             $grid[$y][$x] = true;
         }
 
-        $start = new Point2D(0, 0);
-        $queue = new Queue();
-        $queue->push([$start, 0]);
-        $visited = [];
+        $path = $this->findPath($grid, $size);
 
-        while (!$queue->isEmpty()) {
-            [$point, $steps] = $queue->pop();
-
-            if (isset($visited[$point->y][$point->x])) {
-                continue;
-            }
-
-            $visited[$point->y][$point->x] = true;
-
-            if ($point->x === $size && $point->y === $size) {
-                return $steps;
-            }
-
-            foreach ($point->adjacent() as $adjacent) {
-                if (
-                    $adjacent->x >= 0 && $adjacent->y >= 0
-                    && $adjacent->x <= $size && $adjacent->y <= $size
-                    && !isset($visited[$adjacent->y][$adjacent->x])
-                    && !isset($grid[$adjacent->y][$adjacent->x])
-                ) {
-                    $queue->push([$adjacent, $steps + 1]);
-                }
-            }
-        }
-
-        throw new \RuntimeException('No path found');
+        return count($path, COUNT_RECURSIVE) - count($path) - 1;
     }
 
     public function solvePart2(string $input): string|int
@@ -123,46 +95,64 @@ class Day18 implements DayInterface
         $size = count($lines) < 1024 ? 6 : 70;
         $maxBytes = count($lines) < 1024 ? 12 : 1024;
         $grid = [];
+        $path = null;
 
         for ($i = 0; $i < $maxBytes; $i++) {
-            [$x, $y] = array_map('intval', explode(',', $lines[$i]));
+            [$x, $y] = explode(',', $lines[$i]);
             $grid[$y][$x] = true;
         }
 
         for ($i = $maxBytes; $i < count($lines); $i++) {
-            [$x, $y] = array_map('intval', explode(',', $lines[$i]));
+            [$x, $y] = explode(',', $lines[$i]);
             $grid[$y][$x] = true;
-            $start = new Point2D(0, 0);
-            $queue = new Queue();
-            $queue->push($start);
-            $visited = [];
 
-            while (!$queue->isEmpty()) {
-                $point = $queue->pop();
-
-                if (isset($visited[$point->y][$point->x])) {
-                    continue;
-                }
-
-                $visited[$point->y][$point->x] = true;
-
-                if ($point->x === $size && $point->y === $size) {
-                    continue 2;
-                }
-
-                foreach ($point->adjacent() as $adjacent) {
-                    if (
-                        $adjacent->x >= 0 && $adjacent->y >= 0
-                        && $adjacent->x <= $size && $adjacent->y <= $size
-                        && !isset($visited[$adjacent->y][$adjacent->x])
-                        && !isset($grid[$adjacent->y][$adjacent->x])
-                    ) {
-                        $queue->push($adjacent);
-                    }
-                }
+            if ($path !== null && !isset($path[$y][$x])) {
+                continue;
             }
 
-            return "$x,$y";
+            $path = $this->findPath($grid, $size);
+
+            if ([] === $path) {
+                return "$x,$y";
+            }
         }
+
+        throw new \RuntimeException('No solution found');
+    }
+
+    private function findPath(array $byteGrid, int $size): array
+    {
+        $start = new Point2D(0, 0);
+        $queue = new Queue();
+        $queue->push([$start, []]);
+        $visited = [];
+
+        while (!$queue->isEmpty()) {
+            [$point, $path] = $queue->pop();
+
+            if (isset($visited[$point->y][$point->x])) {
+                continue;
+            }
+
+            $visited[$point->y][$point->x] = true;
+            $path[$point->y][$point->x] = true;
+
+            if ($point->x === $size && $point->y === $size) {
+                return $path;
+            }
+
+            foreach ($point->adjacent() as $adjacent) {
+                if (
+                    $adjacent->x >= 0 && $adjacent->y >= 0
+                    && $adjacent->x <= $size && $adjacent->y <= $size
+                    && !isset($visited[$adjacent->y][$adjacent->x])
+                    && !isset($byteGrid[$adjacent->y][$adjacent->x])
+                ) {
+                    $queue->push([$adjacent, $path]);
+                }
+            }
+        }
+
+        return [];
     }
 }
